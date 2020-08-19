84c89bcf5461c489e46344f3e1fa1b735a2aade6
ReformedCola <reformedcola@gmail.com>
2020-08-19 14:58:11 -0700

    Changed button into Single File Components

---
+++ src/button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="g-button">Button</button>
+</template>
+<script>
+  export default {}
+</script>
+<style lang="scss">
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--button-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+
+    &:hover {
+      border-color: var(--boarder-color-hover);
+    }
+
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
