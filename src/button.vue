f79f198de3dcf9ba81440207e1e7d2f86cb86dde
ReformedCola <reformedcola@gmail.com>
2020-08-19 16:59:08 -0700

    Add icon & iconPosition attributes for button

--- src/button.vue
+++ src/button.vue
@@ -1,17 +1,30 @@
 <template>
-  <button class="g-button">Button</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <span class="content">
+      <slot></slot>
+    </span>
+  </button>
 </template>
 <script>
-  export default {}
+  export default {
+    props: ['icon', 'iconPosition']
+  }
 </script>
 <style lang="scss">
   .g-button {
     font-size: var(--font-size);
     height: var(--button-height);
-    padding: 0 1em;
+    padding: 0 0.6em;
     border-radius: var(--button-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
 
     &:hover {
       border-color: var(--boarder-color-hover);
@@ -23,6 +36,26 @@
 
     &:focus {
       outline: none;
+    }
+
+    > .icon {
+      order: 1;
+      margin-right: .3em;
+    }
+
+    > .content {
+      order: 2;
+    }
+
+    &.icon-right {
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .3em;
+      }
+      > .content {
+        order: 1;
+      }
     }
   }
 </style>
