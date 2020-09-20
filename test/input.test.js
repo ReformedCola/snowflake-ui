d440280110207527e88966c30aa2ca6a8e31def5
ReformedCola <reformedcola@gmail.com>
2020-09-19 20:33:16 -0700

    Add test cases

---
+++ test/input.test.js
@@ -0,0 +1,73 @@
+import Button from '../src/button'
+
+const expect = chai.expect
+import Vue from 'vue'
+import Input from '../src/input'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Input', () => {
+
+  it('should exist', () => {
+    expect(Input).to.exist
+  })
+
+  describe('props', () => {
+    it('could accept value', () => {
+      const Constructor = Vue.extend(Input)
+      const vm = new Constructor({
+        propsData: {
+          value: '1122'
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.value).to.equal('1122')
+      vm.$destroy()
+    })
+
+    it('could accept disabled', () => {
+      const Constructor = Vue.extend(Input)
+      const vm = new Constructor({
+        propsData: {
+          disabled: true
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.disabled).to.equal(true)
+      vm.$destroy()
+    })
+
+    it('could accept readonly', () => {
+      const Constructor = Vue.extend(Input)
+      const vm = new Constructor({
+        propsData: {
+          readonly: true
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.readOnly).to.equal(true)
+      vm.$destroy()
+    })
+
+    it('could accept error', () => {
+      const Constructor = Vue.extend(Input)
+      const vm = new Constructor({
+        propsData: {
+          error: 'You are wrong'
+        }
+      }).$mount()
+      const useElement = vm.$el.querySelector('use')
+      expect(useElement.getAttribute('xlink:href')).to.equal('#i-error')
+      const errorMessage = vm.$el.querySelector('.errorMessage')
+      expect(errorMessage.innerText).to.equal('You are wrong')
+      vm.$destroy()
+    })
+  })
+
+  describe('events', () => {
+    it('supports change event', () => {
+
+    })
+  })
+})
