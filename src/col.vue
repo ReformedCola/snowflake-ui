a414954b427018a43cea5da5f4f9a396aafce166
ReformedCola <reformedcola@gmail.com>
2020-09-24 17:01:09 -0700

    Finish basic row and col

--- src/col.vue
+++ src/col.vue
@@ -1,8 +1,30 @@
 <template>
-  <div class="col">
+  <div class="col" :class="[`col-${span}`]">
     <slot></slot>
   </div>
 </template>
+<script>
+  export default {
+    name: 'SnowCol',
+    props: {
+      span: {
+        type: [Number, String]
+      }
+    }
+  }
+</script>
 <style scoped lang="scss">
+  .col {
+    height: 100px;
+    background: grey;
+    width: 50%;
+    border: 1px solid red;
 
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+  }
 </style>
