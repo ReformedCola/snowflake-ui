74a75d103ad3b98a65c1baa4eb0cba2f21d35fd9
ReformedCola <reformedcola@gmail.com>
2020-10-04 15:48:56 -0700

    Add toast and plugin

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import Header from './header'
 import Sider from './sider'
 import Content from './content'
 import Footer from './footer'
+import Toast from './toast'
+import plugin from './plugin'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -22,6 +24,8 @@ Vue.component('g-header', Header)
 Vue.component('g-sider', Sider)
 Vue.component('g-content', Content)
 Vue.component('g-footer', Footer)
+Vue.component('g-toast', Toast)
+Vue.use(plugin)
 
 new Vue({
   el: '#app',
@@ -32,8 +36,8 @@ new Vue({
     message: 'hi',
   },
   methods: {
-    inputChange(e) {
-      console.log(e.target.value)
+    showToast() {
+      this.$toast('I am message')
     }
   }
 })
---
+++ src/toast.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="toast">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'SnowToast',
+  }
+</script>
+<style lang="scss" scoped>
+  $font-size: 14px;
+  $toast-height: 40px;
+  $toast-bg: rgba(0, 0, 0, 0.75);
+  .toast {
+    font-size: $font-size;
+    line-height: 1.8;
+    height: $toast-height;
+    position: fixed;
+    color: white;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    align-items: center;
+    background: $toast-bg;
+    border-radius: 4px;
+    box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
+    padding: 0 16px;
+  }
+</style>
