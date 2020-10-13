e6a0c9eb05a4a024b2346f71609be1b036813a96
ReformedCola <reformedcola@gmail.com>
2020-10-13 14:24:44 -0700

    Add default styles of popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="popover" @click="onClick" ref="popver">
+  <div class="popover" @click="onClick" ref="popover">
     <div ref="contentWrapper" class="content-wrapper"
          v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display: inline-block;">
       <slot></slot>
     </span>
   </div>
@@ -30,6 +30,12 @@
             (
                 this.$refs.popover === e.target ||
                 this.$refs.popover.contains(e.target)
+            )
+        ) { return }
+        if (this.$refs.contentWrapper &&
+            (
+                this.$refs.contentWrapper === e.target ||
+                this.$refs.contentWrapper.contains(e.target)
             )
         ) { return }
         this.close()
@@ -59,6 +65,8 @@
 </script>
 
 <style lang="scss" scoped>
+  $border-color: #333;
+  $border-radius: 4px;
   .popover {
     display: inline-block;
     vertical-align: top;
@@ -67,8 +75,34 @@
 
   .content-wrapper {
     position: absolute;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    //box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
     transform: translateY(-100%);
+    margin-top: -10px;
+    padding: 0.5em 1em;
+    max-width: 20em;
+    background: white;
+
+    &::before, &::after {
+      content: '';
+      display: block;
+      border: 10px solid transparent;
+      width: 0;
+      height: 0;
+      position: absolute;
+      left: 10px;
+    }
+
+    &::before {
+      border-top-color: black;
+      top: 100%;
+    }
+
+    &::after {
+      border-top-color: white;
+      top: calc(100% - 1px);
+    }
   }
 </style>
