5d502ee76c051a9bd39deeef6d23556cbe6980e8
ReformedCola <reformedcola@gmail.com>
2020-10-13 17:25:17 -0700

    Add first test case for popover

---
+++ test/popover.test.js
@@ -0,0 +1,38 @@
+const expect = chai.expect
+import Vue from 'vue'
+import Popover from '../src/popover'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Popover', () => {
+
+  it('should exist', () => {
+    expect(Popover).to.exist
+  })
+
+  it('could accept position', (done) => {
+    Vue.component('g-popover', Popover)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+      <g-popover position="bottom" ref="a"> 
+        <template slot="content">
+          Popover Content
+        </template>
+        <button>Click me !!</button>
+      </g-popover>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    vm.$nextTick(() => {
+      vm.$el.querySelector('button').click()
+      vm.$nextTick(() => {
+        const {contentWrapper} = vm.$refs.a.$refs
+        expect(contentWrapper.classList.contains('position-bottom')).to.be.true
+        done()
+      })
+    })
+  })
+})
