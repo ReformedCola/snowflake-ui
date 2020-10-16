99d0ca5e07c10ab8b4f05078738dc95d8dbb43e6
ReformedCola <reformedcola@gmail.com>
2020-10-15 19:52:40 -0700

    Use SASS variables for Button

--- src/button.vue
+++ src/button.vue
@@ -3,13 +3,14 @@
           @click="$emit('click')">
     <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
     <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
-    <div class="content">
+    <div class="g-button-content">
       <slot></slot>
     </div>
   </button>
 </template>
 <script>
   import Icon from './icon'
+
   export default {
     name: 'SnowButton',
     components: {
@@ -33,6 +34,15 @@
   }
 </script>
 <style lang="scss" scoped>
+  $font-size: 14px;
+  $button-height: 32px;
+  $button-bg: white;
+  $button-active-bg: #eee;
+  $border-radius: 4px;
+  $color: #333;
+  $border-color: #999;
+  $border-color-hover: #666;
+
   @keyframes spin {
     0% {
       transform: rotate(0deg);
@@ -43,23 +53,23 @@
   }
 
   .g-button {
-    font-size: var(--font-size);
-    height: var(--button-height);
+    font-size: $font-size;
+    height: $button-height;
     padding: 0 0.6em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
+    border-radius: $border-radius;
+    border: 1px solid $border-color;
+    background: $button-bg;
     display: inline-flex;
     justify-content: center;
     align-items: center;
     vertical-align: middle;
 
     &:hover {
-      border-color: var(--border-color-hover);
+      border-color: $border-color-hover;
     }
 
     &:active {
-      background-color: var(--button-active-bg);
+      background-color: $button-active-bg;
     }
 
     &:focus {
@@ -71,7 +81,7 @@
       margin-right: .3em;
     }
 
-    > .content {
+    > .g-button-content {
       order: 2;
     }
 
@@ -82,7 +92,7 @@
         margin-left: .3em;
       }
 
-      > .content {
+      > .g-button-content {
         order: 1;
       }
     }
