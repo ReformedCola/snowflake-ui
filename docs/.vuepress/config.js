668621d770c39505b811230ab755d5799665ef6b
ReformedCola <reformedcola@gmail.com>
2020-10-18 16:40:03 -0700

    Add NavBar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,11 @@ module.exports = {
   title: 'Snowflake UI',
   description: 'A useful UI framework',
   themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Docs', link: '/guide/' },
+      { text: 'GitHub', link: 'https://github.com/ReformedCola/snowflake-ui' },
+    ],
     sidebar: [
       {
         title: 'Getting Started',
